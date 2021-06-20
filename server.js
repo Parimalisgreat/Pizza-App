215ec9c2c116d09670e7942772a31c9bfe410e0d
Parimalisgreat <parimal.jichkar104@gmail.com>
2021-06-20 11:25:26 +0530

    added cart page and authentication

--- server.js
+++ server.js
@@ -10,13 +10,6 @@ const expressLayout =require('express-ejs-layouts');
 
 const PORT = process.env.PORT || 3000 ;
 
-//Assets
-
-app.use(express.static('public'));
-
-app.get('/', (req,res)=>{
-    res.render('home');
-})
 
 //set Template engine
 
@@ -25,6 +18,27 @@ app.use(expressLayout);
 app.set('views',path.join(__dirname,'/resources/views'));
 
 app.set('view engine','ejs')
+
+
+//Assets
+
+app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+    res.render('home');
+})
+
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart');
+})
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login');
+})
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register');
+})
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`);
