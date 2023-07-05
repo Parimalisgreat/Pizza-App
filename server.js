800157be77b08ce66d9336b6931d191917c7f293
Parimalisgreat <parimal.jichkar104@gmail.com>
2023-07-05 12:04:31 +0530

    Fixed errors

--- server.js
+++ server.js
@@ -1,42 +1,47 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express= require('express');
+const express = require("express");
 
 const app = express();
 
-const ejs = require('ejs');
+const ejs = require("ejs");
 
-const path =require('path');
+const path = require("path");
 
-const expressLayout =require('express-ejs-layouts');
+const expressLayout = require("express-ejs-layouts");
 
-const PORT = process.env.PORT || 3000 ;
+const PORT = process.env.PORT || 3000;
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const session = require('express-session')
+const session = require("express-session");
 
-const flash = require('express-flash')
+const flash = require("express-flash");
 
-const MongoDbStore = require('connect-mongo')
+const MongoDbStore = require("connect-mongo");
 
-const passport =require('passport')
-
-
+const passport = require("passport");
 
 //Database connection
 
-const url =process.env.MONGO_CONNECTION_URL
+const url = process.env.MONGO_CONNECTION_URL;
 
-mongoose.connect(url,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:true});
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+});
 
 const connection = mongoose.connection;
 
-connection.on('error', console.error.bind(console, 'connection error:'));
-connection.once('open',()=>{
-    console.log('Database connected...');
-})
-
+connection
+  .once("open", () => {
+    console.log("Database connected...");
+  })
+  .catch((err) => {
+    console.log("Connection failed...");
+  });
 //Session store
 
 // let mongoStore = new MongoDbStore({
@@ -46,51 +51,52 @@ connection.once('open',()=>{
 
 //Session Config
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.COOKIE_SECRET,
-    resave:false,
+    resave: false,
+    saveUnitialized: false,
+
     // store:MongoDbStore.create({mongoUrl:process.env.MONGO_CONNECTION_URL}),
-    client:connection.getClient(),
-    saveUnitialized:false,
-    cookie:{maxAge:1000*60*60*24} //24hrs
-}))
+    client: connection.getClient(),
+
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }, //24hrs
+  })
+);
 
 //Passport config
 
-const passportInit =require('./app/config/passport')
-passportInit(passport)
-app.use(passport.initialize())
-app.use(passport.session())
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(flash())
+app.use(flash());
 
 //Assets
 
-app.use(express.static('public'));
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //Global middleware
 
-app.use((req,res,next)=>{
-    res.locals.session = req.session
-    res.locals.user=req.user
-    next()
-})
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  res.locals.user = req.user;
+  next();
+});
 
 //set Template engine
 
 app.use(expressLayout);
 
-app.set('views',path.join(__dirname,'/resources/views'));
+app.set("views", path.join(__dirname, "/resources/views"));
 
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
 
+require("./routes/web")(app);
 
-
-
-require('./routes/web')(app);
-
-app.listen(PORT, ()=>{
-    console.log(`Listening on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
